487b3592ec5ef76fe95b35bcdb8c4d6128541c0c
Abhishek Maurya <166213264+Abhishekmaurya12367@users.noreply.github.com>
2025-05-31 18:24:09 +0530

    Add files via upload

---
+++ string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int n;
+    char arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%s",arr[i]);
+    }
+
+    for(int i=0;i<n;i++){
+        printf("%s" ,arr[i]);
+    }
+}
