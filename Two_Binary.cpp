436f9926b512f9a726c0e0afe19b6af7b9071a9a
Abhishek Maurya <166213264+Abhishekmaurya12367@users.noreply.github.com>
2025-08-19 21:42:49 +0530

    Add files via upload

---
+++ Two_Binary.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;
+  cin>>t;
+  while(t--){
+    string s1,s2;
+    cin>>s1>>s2;
+    bool flag = false;
+    for(int i=0;i+1<s1.size();i++){
+      if(s1[i]==s2[i] &&  s1[i]=='0' && s1[i+1]==s2[i+1] && s1[i+1]=='1'){
+        flag = true;
+        break; 
+      }
+    }
+    if(flag) {
+      cout<<"YES"<<endl;
+    } else {
+      cout<<"NO"<<endl;
+    }
+  }
+}
